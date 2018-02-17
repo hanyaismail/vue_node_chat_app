fa33351b64142b4f355afb86851827371ebc8835
Ismail <ismail.kerja95@gmail.com>
2018-02-17 16:28:23 +0700

    first socket.io configuration and test

---
+++ client/src/components/Chat.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<p v-if="isConnected">We're connected to the server</p>
+		<p v-else>We're not connected to the server</p>
+		<p>Message from the server: {{socketMessage}}</p>
+		<button @click="pingServer">Ping Server</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			isConnected: false,
+			socketMessage: ''
+		}
+	},
+
+	sockets: {
+		connect() {
+			this.isConnected = true
+		},
+
+		disconnect() {
+			this.isConnected = false
+		},
+
+		messageChannel(data) {
+			this.socketMessage = data
+		}
+	},
+
+	methods: {
+		pingServer: function() {
+			this.$socket.emit('pingServer', 'ping')
+		}
+	}
+}
+</script>
+
+<style>
+	
+</style>
---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(80, function() {
+	console.log('server run on port 80')
+});
+
+io.on('connection', function(socket) {
+	socket.emit('messageChannel', 'Hey you!');
+	socket.on('pingServer', function(data) {
+		console.log(data)
+	})
+})
